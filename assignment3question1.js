7b028ebdfe8f8774f82ef072f4a1d85de25a672b
averyhas <124199288+averyhas@users.noreply.github.com>
2023-03-01 13:32:41 -0500

    Create a custom sprite which moves across the x-axis w/ the user's mouse.
    https://editor.p5js.org/averyhas/sketches/45PEWwS4S

---
+++ assignment3question1.js
@@ -0,0 +1,65 @@
+// Assignment 3, Question 1 - Avery Haskell
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("lightsteelblue");
+
+  noStroke();
+  fill("tan");
+  rect(0, 360, 400, 40); // Ground.
+  drawCat(mouseX, 250, 1);
+}
+
+function drawCat(x, y, size) {
+  push();
+  translate(x, y);
+  scale(size);
+
+  fill("darkgray"); // Tail.
+  noStroke();
+  beginShape();
+  vertex(10, 60);
+  vertex(18, 65);
+  vertex(14, 80);
+  vertex(26, 100);
+  vertex(33, 116);
+  vertex(22, 108);
+  vertex(12, 95);
+  vertex(7, 75);
+  vertex(10, 60);
+  endShape();
+
+  noStroke();
+  fill("gray");
+  ellipse(50, 95, 50, 65); // Body.
+  fill("darkgray");
+  rect(41, 65, 8, 65); // Legs + Paws.
+  rect(50, 65, 8, 65);
+  ellipse(40, 126, 18, 8);
+  ellipse(59, 126, 18, 8);
+  noStroke();
+  fill("darkgray");
+  triangle(22, 18, 26, 45, 44, 30); // Left Ear.
+  triangle(78, 18, 74, 45, 58, 30); // Right Ear.
+
+  fill("darkgray");
+  ellipse(50, 50, 50); // Head.
+  noStroke();
+  fill("white");
+  ellipse(59, 46, 10.5); // Eye. (sclera)
+  fill("gray");
+  ellipse(59, 44.5, 8.5); // Eye. (iris)
+  fill("white");
+  ellipse(41, 46, 10.5); // Eye. (sclera)
+  fill("gray");
+  ellipse(41, 44.5, 9); // Eye. (iris)
+  fill("lightgray");
+  ellipse(43, 60, 20); // Snout.
+  ellipse(57, 60, 20);
+  fill("pink");
+  ellipse(50, 52, 20, 10); // Nose.
+  pop();
+}
